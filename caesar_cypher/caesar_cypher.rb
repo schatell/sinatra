e4f34bce57d7c1a24a20e17d7aa70194a08c7ef5
Annie Deshaies <annie.deshaies@gmail.com>
2017-03-27 19:41:08 -0400

    Completed Cypher

---
+++ caesar_cypher/caesar_cypher.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+cyphered = nil
+to_be_cyphered = nil
+
+get '/' do
+  if params["to_cypher"] != nil
+    to_be_cyphered = params["to_cypher"].to_s
+    cyphered = cypher(params["to_cypher"], params["shift"].to_i)
+  end
+  erb :index ,:locals => {:to_be_cyphered => to_be_cyphered, :cyphered => cyphered}
+end
+
+def cypher(string, number)
+  word = []
+  string.scan(/./) do |i|
+  if("A".."Z").include? (i.upcase)
+    number.times {i = i.next}
+    end
+    word.push(i[-1])
+  end
+  word.join
+end
